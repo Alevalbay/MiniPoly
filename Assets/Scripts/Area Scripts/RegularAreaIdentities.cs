357a1d9d464aebc7fbe0d78912ba4e479c3c8c81
Alevalbay <alisaitakman@gmail.com>
2020-12-29 20:13:15 +0300

    Initial commit

---
+++ Assets/Scripts/Area Scripts/RegularAreaIdentities.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegularAreaIdentities : MonoBehaviour
+{
+    //Identity feature
+    public GameObject areaOwner;
+    public float      areaprice;
+    public float      areaLienPrice;
+    public float      numberOfHouse;
+    public float      numberOfHotel;
+    public string     areaName;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
