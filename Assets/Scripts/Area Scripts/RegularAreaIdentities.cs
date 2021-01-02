bb215f41d353c2199c79b757f49d112146a3992f
Alevalbay <alisaitakman@gmail.com>
2021-01-03 00:32:08 +0300

    Desing Finished All Properities assinged
    Ready for fisrt presantation as cihan said

--- Assets/Scripts/Area Scripts/RegularAreaIdentities.cs
+++ Assets/Scripts/Area Scripts/RegularAreaIdentities.cs
@@ -1,28 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RegularAreaIdentities : MonoBehaviour
 {
     //Identity feature
     public GameObject areaOwner;
-    public float      areaprice;
+    public float      areaPrice;
     public float      areaLienPrice;
     public float      numberOfHouse;
     public float      numberOfHotel;
+    public float      housePrice;
+    public float      hotelPrice;
+    public float      houseRentPrice;
+    public float      hotelRentPrice;
+    public float      areaRentPrice;
+    public float      totalRentPrice;
     public string     areaName;
+    public Text       areaNameText;
+    public Text       areaPriceText;
+    private float totalPayment;
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        areaNameText.text  = areaName;
+        areaPriceText.text = areaPrice.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Function Works for Payment
+    public float CalculatePayment()
+    {
+        totalPayment = ((areaRentPrice) + (numberOfHouse * houseRentPrice) + (numberOfHotel * hotelPrice));
+        return totalPayment;
     }
 }
