5c5154e933547155546191ca8ff73a6017896250
Alevalbay <alisaitakman@gmail.com>
2021-01-03 17:50:45 +0300

    Move Function
    Move Function redesinged but not work well

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public GameObject[] gameTiles;
+    public GameObject[] playersByTurn;
+    public int          activePlayer;
+    public int          currentTile;
+    public bool isCountdownOn;
+    public int diceRoll;
+    void Start()
+    {
+        //Player turn defination function will calls here
+        isCountdownOn = true;
+        //This if for test will change lately
+        activePlayer = 0;
+        currentTile = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        while (isCountdownOn==true)
+        {
+            //First Actived Player
+
+            //Second Calls Dice Roll Phase Function
+
+            //Move Phase Function
+            for (int i = 0; i < diceRoll; i++)
+            {
+                Debug.Log("I value" + i);
+                Debug.Log("Dice Roll:" + diceRoll);
+                //%20 For Stay in array bounds
+                playersByTurn[activePlayer].GetComponent<MoveFunctionScript>().StartMovingFunction(gameTiles[(currentTile + 1)%20].transform);
+                currentTile++;
+                StartCoroutine(Wait2Second());
+            }
+
+            isCountdownOn = false;
+        }
+
+    }
+
+    IEnumerator Wait2Second()
+    {
+        Debug.Log("Started Courtine:"+Time.deltaTime);
+        yield return new WaitForSecondsRealtime(5);
+        Debug.Log("Ended courtine time:"+Time.deltaTime);
+    }
+
+}
--- Assets/Scripts/PlayerFunction/MoveFunctionScript.cs
+++ Assets/Scripts/PlayerFunction/MoveFunctionScript.cs
@@ -8,30 +8,52 @@ public class MoveFunctionScript : MonoBehaviour
     //Variables for testing
     public Transform testTile1,testTile2,TestTile3;
     public Vector3[] testWayPoints;
+    public Vector3[] wayPointsToNextTile;
     private Vector3 testVector1, testVector2, testVector3;
+    private Vector3 goesUpVector, goesForwardVector, goesDownVector;
+
+    private float timer;
     void Start()
     {
-        testVector1.x = transform.position.x; testVector1.y = transform.position.y + 1; testVector1.z = transform.position.z;
+      /*  testVector1.x = transform.position.x; testVector1.y = transform.position.y + 1; testVector1.z = transform.position.z;
         testVector2.x = testTile1.position.x; testVector2.y = transform.position.y;     testVector2.z = testTile1.transform.position.z;
         testVector3.x = testTile1.position.x; testVector3.y = 0.5f;testVector3.z= testTile1.transform.position.z;
 
-        testWayPoints = new Vector3[] {testVector1,testVector2,testVector3};
-       
-        StartMovingFunction(testTile1);
+        testWayPoints = new Vector3[] { testVector1, testVector2, testVector3 };
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
 
     //Function to work for moving.Uses parameters nexttile and dice result
     public void StartMovingFunction(Transform nextTile)
     {
-        transform.DOPath(testWayPoints,2);
-       
-      
+        //We need 3 waypoint first object will move up,then goes forward next tile,last one move down
+        MakingWayPoints(nextTile);
+
+        //Dotween asset function makes moving easy
+        transform.DOPath(wayPointsToNextTile,2);
+  
     }
+
+    //We need 3 waypoint first object will move up,then goes forward next tile,last one move down
+    //This function converts tranform to Vector3
+    public void MakingWayPoints(Transform nextTile)
+    { 
+        goesUpVector.x      = transform.position.x ; goesUpVector.y      = transform.position.y + 1.5f ; goesUpVector.z      = transform.position.z;
+        goesForwardVector.x = nextTile.position.x  ; goesForwardVector.y = transform.position.y        ; goesForwardVector.z = nextTile.transform.position.z;
+        goesDownVector.x    = nextTile.position.x  ; goesDownVector.y    = 0.5f                        ; goesDownVector.z    = nextTile.transform.position.z;
+
+        wayPointsToNextTile = new Vector3[] { goesUpVector, goesForwardVector, goesDownVector };
+
+    }
+
+  
+
+
+
 }
